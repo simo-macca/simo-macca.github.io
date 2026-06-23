2d53944bb08ebe86c863c86996acd5dce54ee234
Simone <simone.maccario04@gmail.com>
2026-06-23 16:23:26 +0200

    fix url path

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/my-portfolio/',
+  base: '/',
   plugins: [
     vue(),
     vueDevTools(),
